1fc04f595f4b988d5840337bdfe4f2642564ed18
VENHRUN_PETRO <venhrun@pozitiff.ua>
2018-02-15 23:53:23 +0200

    'add_file'

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+/*slider*/
+$('.sl').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                arrows: false,
+                slidesToShow: 1
+            }
+        },]
+});
+/*slider*/
